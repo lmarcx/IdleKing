d125c3d1656a6a3e5b6e2cc876dd91b57fe700df
Loïc <lmarcx.pro@gmail.com>
2026-02-17 20:06:47 +0100

    kingamas test ok

--- packages/game-core/index.ts
+++ packages/game-core/index.ts
@@ -1,3 +1,4 @@
 export * as power from "./power/index.js";
 export * as loot from "./loot/index.js";
 export * as progression from "./progression/index.js";
+export * as economy from "./economy/index.js";
