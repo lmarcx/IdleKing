a657f6ee8b5a4f7472b41b129443728a6546b3a4
Loïc <lmarcx.pro@gmail.com>
2026-02-18 19:05:48 +0100

    progress xp global basic init + indexes

--- packages/game-core/progression/index.ts
+++ packages/game-core/progression/index.ts
@@ -1,3 +1,7 @@
 export * from "./baseStats.js";
 export * from "./expectedIlvl.js";
 export * from "./xpCurve.js";
+export * from "./age.js";
+export * from "./worldXp.js";
+export * from "./sources.js";
+export * from "./xp.js";
